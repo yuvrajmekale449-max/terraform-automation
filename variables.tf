f4921727800cb5cec9e66113d9f754a373ae5cad
Yuvraj Mekale <yuvrajmekale449@gmail.com>
2026-05-25 16:04:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "yuvi.pem" 
+    default     =  "yuvi-key" 
 }
 
 variable "instance_type" { 
