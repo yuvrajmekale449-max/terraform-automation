b9071f2325ba7ff981702a81bace3d00be64a9a7
Yuvraj Mekale <yuvrajmekale449@gmail.com>
2026-05-25 19:28:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "young-minds-app-project-terraform-state-18"
     key = "main"
-    region = "us-east-1"
+    region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
